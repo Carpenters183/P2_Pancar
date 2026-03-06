32ffa0824478da72ac3277626f9b67ee827410f2
Carpenters183 <carpanbrun@gmail.com>
2026-03-06 13:55:13 +0700

    update login register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,55 +2,39 @@
 <html>
 
 <head>
-	<!-- Basic Page Info -->
 	<meta charset="utf-8">
-	<title>DeskApp - Bootstrap Admin Dashboard HTML Template</title>
+	<title>Register | Aplikasi</title>
 
-	<!-- Site favicon -->
 	<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/vendors/images/apple-touch-icon.png') }}">
 	<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/vendors/images/favicon-32x32.png') }}">
 	<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/vendors/images/favicon-16x16.png') }}">
 
-	<!-- Mobile Specific Metas -->
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-	<!-- Google Font -->
 	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
-	<!-- CSS -->
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets/vendors/styles/core.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets/vendors/styles/icon-font.min.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/src/plugins/jquery-steps/jquery.steps.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets/vendors/styles/style.css') }}">
-
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119386393-1"></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag('js', new Date());
-
-		gtag('config', 'UA-119386393-1');
-	</script>
 </head>
 
 <body class="login-page">
+
 	<div class="login-header box-shadow">
 		<div class="container-fluid d-flex justify-content-between align-items-center">
 			<div class="brand-logo">
-				<a href="login.html">
+				<a href="{{ route('landingpage') }}">
 					<img src="{{ asset('assets/vendors/images/deskapp-logo.svg') }}" alt="">
 				</a>
 			</div>
 			<div class="login-menu">
 				<ul>
-					<li><a href="login.html">Login</a></li>
+					<li><a href="{{ url('/login') }}">Login</a></li>
 				</ul>
 			</div>
 		</div>
 	</div>
-	
 
-    <div class="login-wrap d-flex align-items-center flex-wrap justify-content-center">
+	<div class="login-wrap d-flex align-items-center flex-wrap justify-content-center">
 		<div class="container">
 			<div class="row align-items-center">
 				<div class="col-md-6 col-lg-7">
@@ -61,32 +45,36 @@
 						<div class="login-title">
 							<h2 class="text-center text-primary">Register</h2>
 						</div>
-						<form action="{{ route('login.process') }}" method="POST">
-                                 @csrf
-								<section>
-									<div class="form-wrap max-width-600 mx-auto">
-										<div class="form-group row">
-											<label class="col-sm-4 col-form-label">Email Address*</label>
-											<div class="col-sm-8">
-												<input type="email" name="email" placeholder="Email" class="form-control">
-											</div>
-										</div>
-										<div class="form-group row">
-											<label class="col-sm-4 col-form-label">Username*</label>
-											<div class="col-sm-8">
-												<input type="text" name="name" placeholder="Nama" class="form-control">
-											</div>
-										</div>
-										<div class="form-group row">
-											<label class="col-sm-4 col-form-label">Password*</label>
-											<div class="col-sm-8">
-												<input type="password" name="password" placeholder="Password" class="form-control">
-											</div>
-										</div>
-                                        <button type="submit" class="btn btn-primary">Register</button>
-									</div>
-								</section>
-								</div>
+
+						<form action="{{ route('jemaah.register.submit') }}" method="POST">
+							@csrf
+
+							<div class="input-group custom">
+								<input type="text" name="username" class="form-control form-control-lg" placeholder="Username" required>
+							</div>
+
+							<div class="input-group custom">
+								<input type="email" name="email" class="form-control form-control-lg" placeholder="Email Address" required>
+							</div>
+
+							<div class="input-group custom">
+								<input type="password" name="password" class="form-control form-control-lg" placeholder="Password" required>
+							</div>
+
+							<div class="input-group custom">
+								<input type="password" name="password_confirmation" class="form-control form-control-lg" placeholder="Confirm Password" required>
+							</div>
+
+							<button type="submit" class="btn btn-primary btn-lg btn-block">
+								Sign Up
+							</button>
+
+							<div class="font-16 weight-600 pt-10 pb-10 text-center" data-color="#707373">OR</div>
+
+							<div class="input-group mb-0">
+								<a class="btn btn-outline-primary btn-lg btn-block" href="{{ url('/login') }}">
+									Back To Login
+								</a>
 							</div>
 						</form>
 					</div>
@@ -94,13 +82,10 @@
 			</div>
 		</div>
 	</div>
-	<!-- success Popup html End -->
-	<!-- js -->
+
 	<script src="{{ asset('assets/vendors/scripts/core.js') }}"></script>
 	<script src="{{ asset('assets/vendors/scripts/script.min.js') }}"></script>
 	<script src="{{ asset('assets/vendors/scripts/process.js') }}"></script>
 	<script src="{{ asset('assets/vendors/scripts/layout-settings.js') }}"></script>
-	<script src="{{ asset('assets/src/plugins/jquery-steps/jquery.steps.js') }}"></script>
-	<script src="{{ asset('assets/vendors/scripts/steps-setting.js') }}"></script>
 </body>
 </html>
